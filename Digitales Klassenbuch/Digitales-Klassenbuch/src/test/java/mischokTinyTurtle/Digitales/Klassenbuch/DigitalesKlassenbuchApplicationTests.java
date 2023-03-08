8389eeaa9ebe4cc4ad6ad54820daa0d5d7130e29
AlisaDappa <120105224+Alisa-Dappa@users.noreply.github.com>
2023-03-08 10:15:57 +0100

    Add files via upload
    initialisierte Datei

---
+++ Digitales Klassenbuch/Digitales-Klassenbuch/src/test/java/mischokTinyTurtle/Digitales/Klassenbuch/DigitalesKlassenbuchApplicationTests.java
@@ -0,0 +1,13 @@
+package mischokTinyTurtle.Digitales.Klassenbuch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalesKlassenbuchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
