daa5ae86a094c353d0fbb1dc9c43f88daf54c26f
Alisa Dappa <alisa.dappa@tn.mischok.academy>
2023-03-14 10:31:34 +0100

    Tabellen wurden in Datenbank erstellt

--- src/main/java/com/example/maventest/MavenTestApplication.java
+++ src/main/java/com/example/maventest/MavenTestApplication.java
@@ -1,5 +1,7 @@
 package com.example.maventest;
 
+import com.example.maventest.Model.Modul;
+import com.example.maventest.Model.Themen;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -14,15 +16,25 @@ public class MavenTestApplication {
 		//Treiber einbinden
 		try {
 			Class.forName("org.postgresql.Driver");
-		}catch (java.lang.ClassNotFoundException e) {
+		} catch (java.lang.ClassNotFoundException e) {
 			throw new RuntimeException(e);
 		}
 
 		//Verbinden
-			String url = "jdbc:postgresql://mouse.db.elephantsql.com/fsoypwho";
-			String user = "fsoypwho";
-			String password = "6WN1ECo6__Z6frAjXrPcnmkYiYIDpIqg";
-			Connection connection;
-			connection = DriverManager.getConnection(url, user, password);
+		String url = "jdbc:postgresql://mouse.db.elephantsql.com/fsoypwho";
+		String user = "fsoypwho";
+		String password = "6WN1ECo6__Z6frAjXrPcnmkYiYIDpIqg";
+		Connection connection;
+		connection = DriverManager.getConnection(url, user, password);
+
+		Modul modul = new Modul();
+		Themen themen = new Themen();
+
+		//Erstellung der Tabellen in ElephantSQL
+		Statement st = connection.createStatement();
+		ResultSet rs = null;
+
+		st.execute("CREATE TABLE IF NOT EXISTS MODUL (ID SERIAL PRIMARY KEY, NAME VARCHAR(50))");
+		st.execute("CREATE TABLE IF NOT EXISTS THEMEN (ID SERIAL PRIMARY KEY, MODUL_ID INTEGER, BEZEICHNUNG VARCHAR(50))");
 		}
 	}
