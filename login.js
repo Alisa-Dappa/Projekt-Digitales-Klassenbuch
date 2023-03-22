72116d6345adcaa0246e1f3bc4571adc59ff30ae
Sebastian Asum <Sebastian.Asum@tn.mischok.academy>
2023-03-22 09:43:26 +0100

    small changes

--- login.js
+++ login.js
@@ -1,32 +1,26 @@
-// Importieren der MSAL-Bibliothek
-import * as msal from '@azure/msal-browser';
-
-
-
-// Konfigurieren von MSAL
-const msalConfig = {
-  auth: {
-    clientId: 'your-client-id',
-    authority: 'https://login.microsoftonline.com/your-tenant-id',
-  },
-  cache: {
-    cacheLocation: 'sessionStorage',
-  },
+var config = {
+  tenant: 'f8cdef31-a31e-4b4a-93e4-5f571e91255a',
+  clientId: 'add8085c-a84e-4db7-9976-83622f1cc8cb',
+  redirectUri: window.location.origin,
+  postLogoutRedirectUri: window.location.origin,
+  cacheLocation: 'localStorage'
 };
-const msalInstance = new msal.PublicClientApplication(msalConfig);
-
-// Anmelden des Benutzers
-msalInstance.loginPopup()
-  .then(() => {
-    // Abrufen eines Zugriffstokens
-    return msalInstance.acquireTokenSilent({
-      scopes: ['user.read'],
-    });
-  })
-  .then((accessTokenResponse) => {
-    // Speichern des Tokens in der Session
-    sessionStorage.setItem('accessToken', accessTokenResponse.accessToken);
-  })
-  .catch((error) => {
-    console.log(error);
+var authContext = new AuthenticationContext(config);
+var isCallback = authContext.isCallback(window.location.hash);
+if (isCallback) {
+  authContext.handleWindowCallback();
+}
+var user = authContext.getCachedUser();
+if (!user) {
+  authContext.login();
+}
+function getToken() {
+  authContext.acquireToken('your_resource_id', function (error, token) {
+    if (error || !token) {
+      console.log('Error retrieving token: ', error);
+    } else {
+      // Use the token to authenticate the user
+      console.log('Token: ', token);
+    }
   });
+}
